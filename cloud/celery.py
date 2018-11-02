38e47c08b0840a6f01aaffd06b73af0292377bee
mohsen <mohsen.nazari85@gmail.com>
2018-11-02 12:13:23 +0330

    update redis db select for cache and celery

--- cloud/celery.py
+++ cloud/celery.py
@@ -5,6 +5,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cloud.settings')
 app = Celery('cloud')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 #http://docs.celeryproject.org/en/latest/getting-started/brokers/redis.html
-app.conf.broker_url = 'redis://:mohsen@127.0.0.1:6379/0'
+app.conf.broker_url = 'redis://:mohsen@127.0.0.1:6379/3'
 app.autodiscover_tasks()
 
--- cloud/settings.py
+++ cloud/settings.py
@@ -182,7 +182,7 @@ CKEDITOR_ALLOW_NONIMAGE_FILES = True
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://:mohsen@127.0.0.1:6379/1",
+        "LOCATION": "redis://:mohsen@127.0.0.1:6379/2",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient"
         },
